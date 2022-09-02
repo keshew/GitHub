a76c442277f8cc2192da5de93c10d21e024bc102
keshew <artyom.korotckov2017@yandex.ru>
2022-09-02 19:45:51 +0500

    Initial Commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Артём Коротков on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
